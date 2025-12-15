5417d8bee99b23fd28b7d79f873eff45365f058e
berkee25 <berkeozdemir25@gmail.com>
2025-12-15 19:48:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        BasicTree tree = new BasicTree();
+
+        // Tree creation
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.print("Preorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nInorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+
+        System.out.println("\nHeight: " + tree.height(tree.root));
+
+        System.out.println(
+                "Depth of node 5: " +
+                        tree.depth(tree.root, tree.root.left.right, 0)
+        );
+    }
 }
